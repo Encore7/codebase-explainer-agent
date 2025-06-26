7d565b33b59face87b1dd10c476ce5af9ddd4307
Encore7 <amankumar5150@gmail.com>
2025-06-26 21:52:21 +0200

    Router

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 
+from backend.app.api.endpoints.auth import router as auth_router
 from backend.app.api.endpoints.health import router as health_router
 
 router = APIRouter()
 
 # Public
 router.include_router(health_router, prefix="/health", tags=["Health"])
+router.include_router(auth_router, prefix="/auth", tags=["Auth"])
